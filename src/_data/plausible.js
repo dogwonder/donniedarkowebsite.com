e0234f09cb456f5cd0909042f7170ec2dfd71c84
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2025-01-08 16:47:48 +0000

    New stats

--- src/_data/plausible.js
+++ src/_data/plausible.js
@@ -1,14 +1,16 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
+//See more here: https://www.jjude.com/tech-notes/plausible-and-11ty/
+
 module.exports = async function () {
     try {
 
-         // Determine the authorization key based on the environment
-         const authKey = process.env.PLAUSIBLE_KEY;
-
+        // Determine the authorization key based on the environment
+        const authKey = process.env.PLAUSIBLE_KEY;
+        const siteKey = process.env.PLAUSIBLE_SITE;
 
         // https://developer.github.com/v3/repos/#get
-        let json = await EleventyFetch("https://plausible.io/api/v1/stats/aggregate/?site_id=donniedarkowebsite.com&period=12mo", {
+        let json = await EleventyFetch(`https://plausible.io/api/v1/stats/aggregate/?site_id=${siteKey}&period=12mo`, {
             duration: "1d", // 1 day
             type: "json", // also supports "text" or "buffer"
             fetchOptions: {
